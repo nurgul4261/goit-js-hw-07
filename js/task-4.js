0fc826d277d767ebf00558190bd3e246a3b252f6
nurgul <nurguloksuzoglu@gmail.com>
2025-09-20 00:06:22 +0300

    revised

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
 document.querySelector('form.login-form').addEventListener('submit', function(event) {
     const email = event.target.elements.email.value.trim();
     const password = event.target.elements.password.value.trim();
-    console.log(`Email: ${email}, Password: ${password}`);
+    
     if (email === '' || password === '') {
         event.preventDefault();
         alert('All fields must be filled!');
