7a48a8b0b077f880017f13c0b8bcf65fc6ad5fc8
nurgul <nurguloksuzoglu@gmail.com>
2025-09-19 19:43:40 +0300

    revised

--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,9 @@
 document.querySelector('form.login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
     const email = event.target.elements.email.value.trim();
     const password = event.target.elements.password.value.trim();
     console.log(`Email: ${email}, Password: ${password}`);
     if (email === '' || password === '') {
+        event.preventDefault();
         alert('All fields must be filled!');
     } else {
         const formData = { email, password };
